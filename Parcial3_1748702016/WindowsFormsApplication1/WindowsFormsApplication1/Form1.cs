69bf1571c2e9cbb3613afb82962e85480a91549f
danihdez96 <47672374+danihdez96@users.noreply.github.com>
2019-04-23 20:02:53 -0600

    Add files via upload

---
+++ Parcial3_1748702016/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        string Usuario, Contraseña;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Usuario != "ADMIN" || Contraseña != "admin1234")
+            {
+                MDIParent1 obj = new MDIParent1();
+                obj.Show();
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Parcial3_1748702016/WindowsFormsApplication1/WindowsFormsApplication1/MDIParent1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class MDIParent1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.informacionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informacionEstudianteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.operacionesMatematicasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.informacionToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(632, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // informacionToolStripMenuItem
+            // 
+            this.informacionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.informacionEstudianteToolStripMenuItem,
+            this.operacionesMatematicasToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.informacionToolStripMenuItem.Name = "informacionToolStripMenuItem";
+            this.informacionToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.informacionToolStripMenuItem.Text = "Ventanas";
+            // 
+            // informacionEstudianteToolStripMenuItem
+            // 
+            this.informacionEstudianteToolStripMenuItem.Name = "informacionEstudianteToolStripMenuItem";
+            this.informacionEstudianteToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+            this.informacionEstudianteToolStripMenuItem.Text = "Informacion estudiante";
+            this.informacionEstudianteToolStripMenuItem.Click += new System.EventHandler(this.informacionEstudianteToolStripMenuItem_Click);
+            // 
+            // operacionesMatematicasToolStripMenuItem
+            // 
+            this.operacionesMatematicasToolStripMenuItem.Name = "operacionesMatematicasToolStripMenuItem";
+            this.operacionesMatematicasToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+            this.operacionesMatematicasToolStripMenuItem.Text = "Operaciones matematicas";
+            this.operacionesMatematicasToolStripMenuItem.Click += new System.EventHandler(this.operacionesMatematicasToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // MDIParent1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 453);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.Name = "MDIParent1";
+            this.Text = "MDIParent1";
+            this.Load += new System.EventHandler(this.MDIParent1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem informacionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informacionEstudianteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem operacionesMatematicasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+
+    }
+}
+
+
+
